fa00a599ca45835da2f684109820ad063aa2177b
GWYOG <jjn1jjn2@163.com>
2016-10-28 21:45:50 +0800

    Recipe tweaks

--- src/main/java/pers/gwyog/gtveinlocator/ModItems.java
+++ src/main/java/pers/gwyog/gtveinlocator/ModItems.java
@@ -2,9 +2,11 @@ package pers.gwyog.gtveinlocator;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
+import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_ModHandler;
+import gregtech.api.util.GT_OreDictUnificator;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import pers.gwyog.gtveinlocator.compat.LoadedModHelper;
@@ -38,21 +40,31 @@ public class ModItems {
     		GameRegistry.registerItem(itemVeinLocator, "veinLocator");
         	if (!ModConfig.recipeVeinLocatorDisabled) 
         		GT_ModHandler.addCraftingRecipe(new ItemStack(ModItems.itemVeinLocator), GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE, new Object[]{
-        				"SSS", "SwS", "SRS",
-        				Character.valueOf('S'), OrePrefixes.plate.get(Materials.Steel),
-        				Character.valueOf('R'), OrePrefixes.plate.get(Materials.RedAlloy)
+        			"SWS", "WwW", "SCS",
+        			Character.valueOf('S'), OrePrefixes.plate.get(Materials.Steel),
+        			Character.valueOf('W'), GT_OreDictUnificator.get(OrePrefixes.cableGt01, Materials.Tin, 1L),
+        			Character.valueOf('C'), ItemList.Circuit_Basic.get(1L, new Object[0])
         		});
     	}
     	if (ModConfig.advancedVeinLocatorEnabled) {
     		GameRegistry.registerItem(itemAdvancedVeinLocator, "advancedVeinLocator");
     		if (!ModConfig.recipeAdvancedVeinLocatorDisabled)
-        		GT_ModHandler.addCraftingRecipe(new ItemStack(ModItems.itemAdvancedVeinLocator), GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE, new Object[]{
-        				"AAA", "AwA", "ACA",
-        				Character.valueOf('A'), OrePrefixes.plate.get(Materials.Aluminium),
-        				Character.valueOf('C'), OrePrefixes.circuit.get(Materials.Basic)
-        		});
+    			GT_ModHandler.addCraftingRecipe(new ItemStack(ModItems.itemAdvancedVeinLocator), GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE, new Object[]{
+    				"AWA", "WwW", "ACA",
+    				Character.valueOf('A'), OrePrefixes.plate.get(Materials.Aluminium),
+    				Character.valueOf('W'), GT_OreDictUnificator.get(OrePrefixes.cableGt02, Materials.AnyCopper, 1L),
+    				Character.valueOf('C'), ItemList.Circuit_Good.get(1L, new Object[0])
+    			});
     	}
-    	if (ModConfig.eliteVeinLocatorEnabled)
+    	if (ModConfig.eliteVeinLocatorEnabled) {
     		GameRegistry.registerItem(itemEliteVeinLocator, "eliteVeinLocator");
+    		if (!ModConfig.recipeEliteVeinLocatorDisabled)
+    			GT_ModHandler.addCraftingRecipe(new ItemStack(ModItems.itemEliteVeinLocator), GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE, new Object[]{
+        				"SWS", "WwW", "SCS",
+        				Character.valueOf('S'), OrePrefixes.plate.get(Materials.StainlessSteel),
+        				Character.valueOf('W'), GT_OreDictUnificator.get(OrePrefixes.cableGt04, Materials.Silver, 1L),
+        				Character.valueOf('C'), ItemList.Circuit_Advanced.get(1L, new Object[0])
+        			});
+    	}
     }
 }
